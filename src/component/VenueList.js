7c5860da8a8222f4201ba3fb1e415365b100d7b8
cfochoa7 <cf_ochoa@hotmail.com>
2018-11-08 14:00:07 -0700

    Update VenueList.js

--- src/component/VenueList.js
+++ src/component/VenueList.js
@@ -4,6 +4,8 @@ import ListItem  from './ListItem';
 class VenueList extends Component {
   render() {
 
+/*The variable bridge establishes the <ul> by using .map() that will list every <li> to the user. The key identifies each venue's title.
+The 'click' will have the data passed from the venue.venue and link it with the listed venue the user clicks on to the marker on the map.*/
     let connect = this.props;
     let bridge;
      bridge = <ul
@@ -13,7 +15,7 @@ class VenueList extends Component {
                     <ListItem
                         key={venue.venue.name}
                         venue={venue}
-                        /*Assisted by Mayguen Ojeda from FEND-proj-7*/
+                        //Assisted by Mayguen Ojeda from FEND-proj-7
                         click={ () =>
                         connect.press(venue.venue)
                         }
