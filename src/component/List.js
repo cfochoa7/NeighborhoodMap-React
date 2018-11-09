63c8db1099b106deb5d2e5a53c19eed6ec771b16
cfochoa7 <cf_ochoa@hotmail.com>
2018-11-08 20:29:17 -0700

    Update List.js

--- src/component/List.js
+++ src/component/List.js
@@ -5,6 +5,7 @@ class List extends Component {
 
 /* The <li> has an onclick method that will link to the props 'click' in Venue.js allowing the user to click on the listed venues.
 The listed venue's display to the user is accessed through the venue's 'venue.venue.name'.*/
+// Partially assisted by Jason Michael White [FEND] Project Coach 
 let connect = this.props;
 let access;
       access = <li
